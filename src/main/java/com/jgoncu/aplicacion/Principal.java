9d04a3387798603117dc129a08b94fb66b7dfb8d
JGoncu97 <jggc1197@gmail.com>
2024-10-25 11:29:12 -0500

    Inicio de practica

---
+++ src/main/java/com/jgoncu/aplicacion/Principal.java
@@ -0,0 +1,10 @@
+package com.jgoncu.aplicacion;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		Aplicacion miAplicacion=new Aplicacion();
+		miAplicacion.iniciar();
+		}
+
+}
